4054e12c97b10d7ea244833ab54289479b92ef45
SomebodyGit <113550545+SomebodyGit@users.noreply.github.com>
2023-07-09 00:07:03 +0300

    Update Kafka_consumer.py

--- Kafka_consumer.py
+++ Kafka_consumer.py
@@ -1,12 +1,13 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer 
 
 def get_data_from_kafka(topic):
+    
+    consumer = KafkaConsumer(topic, bootstrap_servers='localhost:9092') 
+    while (True): 
+        messages = consumer.poll(timeout_ms=1000)
+        for tp, messages in messages.items(): 
+            for message in messages: 
+                print(message.value.decode('utf-8')) 
 
-    consumer = KafkaConsumer(topic, bootstrap_servers='localhost:9092')
-
-    for message in consumer:
-        return message.value.decode('utf-8')
-
-topic = 'quickstart-events1'
-data = get_data_from_kafka(topic)
-print(data)
+topic = 'quickstart-events1' 
+get_data_from_kafka(topic)
